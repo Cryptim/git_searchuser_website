946dbbddc441b90197cb271c914c0282509d46df
timothy <timothyignatiusa@gmail.com>
2023-06-23 04:44:07 +0100

    git_user

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Infos, Repos, User, Search, Navbar } from "../component";
+import loadingImages from "../images/preloader.gif";
+import { GithubContext } from "../content/content";
+const Dashboard = () => {
+  return (
+    <main>
+      <h2>DashBoard Page</h2>
+    </main>
+  );
+};
+export default Dashboard;
